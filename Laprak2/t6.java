aeeaa548c244de57fbc7ffcf3c080bb0a23a41c5
efrizall <rizal.ef340@gmail.com>
2019-10-06 14:01:09 +0700

    Tugas Laprak 2

---
+++ Laprak2/t6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class t6{
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+
+		int nilai = 1;
+
+		while(nilai != 0){
+            System.out.print("Masukan angka: ");
+            nilai = input.nextInt();
+        }
+	}
+}
