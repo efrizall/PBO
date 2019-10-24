0cd69671aa41185c1bae0050706232477e04808b
efrizall <rizal.ef340@gmil.com>
2019-10-24 10:05:54 +0700

    Quis PBO

---
+++ Quis_PBO_24-10-2019.java
@@ -0,0 +1,37 @@
+public class Efrizal_0110218024{
+	public static void main(String[] args) {
+
+		// values[0] = 10;
+		// values[9] = 10;
+
+		// for (int i=0; i<=4; i++){
+		// 	System.out.print(primes[i] + " , ");
+		// }
+
+
+		// No 2
+		// for (int i = 0; i < 2; i++)
+		// {
+		// 	primes[4 - i] = primes[i];
+		// }
+
+		// No 3
+		// for (int i = 0; i < 5; i++)
+		// {
+		// 	primes[i]++;
+		// }
+
+		int[] values = {1,2,3,-4,5,6,7,-8,9,-10,-203};
+
+		int maks = 0;
+
+		for (int i=0; i<values.length; i++){
+			maks++;
+		}
+		System.out.print(maks);
+
+		// for (int i = 0; i < values.length; i++){
+		// 	System.out.print(values[i] + " , ");
+		// }
+	}
+}
